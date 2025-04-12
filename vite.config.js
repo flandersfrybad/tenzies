7372296a2b6f1fe04b8e886593f2c4900ab247ce
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-04-11 23:42:40 -0600

    arg

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' && process.env.VITE_APP_BASE_URL 
-    ? process.env.VITE_APP_BASE_URL 
-    : '/',
+  base: process.env.GITHUB_ACTIONS ? '/tenzies/' : '/',
 })
